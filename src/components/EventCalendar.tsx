4a402d1d9223120b03655aecdf82aa542c1293d3
krtt1 <krittitadsr@gmail.com>
2025-09-14 21:21:37 +0700

    แก้ไขหน้าCalendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -5,19 +5,26 @@ import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css'
 
 type ValuePiece = Date | null;
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-
 const EventCalendar = () => {
+  const [value, onChange] = useState<Value>(new Date());
 
-    const [value, onChange] = useState<Value>(new Date());
-
-    return (
-        <div className="bg-white p-4 rounded-md">
-            <Calendar onChange={onChange} value={value} />
-        </div>
-    )
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <Calendar
+        onChange={onChange}
+        value={value}
+        locale="en-GB" // ตั้ง locale ให้ตรง server & client
+        formatDay={(locale, date) =>
+          date.toLocaleDateString('en-GB', {
+            day: 'numeric',
+            
+          })
+        }
+      />
+    </div>
+  )
 }
 
-export default EventCalendar
+export default EventCalendar;
